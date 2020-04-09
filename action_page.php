9eb382be629051135a9e4eddd6d94527ba2f03bb
gesab001 <144000giovanni@gmail.com>
2020-04-10 01:14:38 +1200

    first commit

---
+++ action_page.php
@@ -0,0 +1,27 @@
+ <?php
+
+include "../../inc/dbinfo.inc";
+
+$comment = "Father, which art in heaven "  .  $_POST["comment"]  .  " in the name of Jesus, Amen" ;
+echo $comment;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO pray (item)
+VALUES ('$comment')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+header('Location: index.php');
+?> 
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Prayer</h1>
+
+
+<br>
+<p>Father which art in heaven, </p>
+<textarea rows="4" cols="50" name="comment" form="usrform">
+</textarea>
+</p>in Jesus name, Amen.</p>
+
+<form action="/prayer.onecloudapps.net/action_page.php" id="usrform" method="post">
+  <input type="submit" value="Add">
+</form>
+</body>
+</html>
+
+
